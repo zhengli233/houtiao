61a47e070a9894d5147d28504d80ef8fefb9a3d3
huaxiaocheng <huaxiaocheng@anosi.cn>
2019-11-26 15:22:15 +0800

    猴调1.0.1 axios方法模板及代码规范

--- houtiao/src/components/FindHoutiao.vue
+++ houtiao/src/components/FindHoutiao.vue
@@ -1,25 +1,25 @@
 <template>
-    <div id="FindHoutiao">
-    <h1>{{ houtiao_name }}</h1>
-    <input v-model="houtiao_name"><br>
-    <button v-on:click="getHoutiao">找到猴调的人</button>
+    <div>
+    <h1>{{ monkeyTwigName }}</h1>
+    <button v-on:click="getMonkeyTwig()">找到猴调的人</button>
     </div>
 </template>
 
 <script>
 export default {
-  name: 'FindHoutiao',
   data () {
     return {
-      houtiao_name: 'Hua Xiaocheng'
+      monkeyTwigName: 'Hua Xiaocheng'
     }
   },
   methods: {
-    getHoutiao: function () {
-      this.axios.get('/houtiao_person').then((response) => {
-        this.houtiao_name = response.json()
-      }, (response) => {}
-      )
+    getMonkeyTwig: function () {
+      this.axios.get('/houtiao_person').then(res => {
+        console.log('猴调，对的', res)
+        this.monkeyTwigName = res.data.data.name
+      }).catch(err => {
+        console.log('猴调，出错了', err)
+      })
     }
   }
 }
