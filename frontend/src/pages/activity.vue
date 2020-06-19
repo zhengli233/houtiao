a221b9cd0e55d8ee2df0787716d6dbb0e0d7e33c
huaxiaocheng <huaxiaocheng@anosi.cn>
2020-06-19 17:08:26 +0800

    活动更新

--- frontend/src/pages/activity.vue
+++ frontend/src/pages/activity.vue
@@ -29,7 +29,7 @@ export default {
       tableData: [
         {id: 1, name: '暗黑神殿', weekday: [1, 2, 3, 4, 5, 6, 7], type: '日常'},
         {id: 2, name: '痛苦地下室', weekday: [1, 2, 3, 4, 5, 6, 7], type: '日常'},
-        {id: 3, name: '眩惑之塔', weekday: [1, 2, 3, 4, 5, 6, 7], type: '日常'},
+        {id: 3, name: '云上长安战未央', weekday: [1, 2, 3, 4, 5, 6, 7], type: '日常'},
         {id: 4, name: '第1狱', weekday: [1, 2, 3, 4, 5, 6, 7], type: '日常'},
         {id: 5, name: '第5狱', weekday: [1, 2, 3, 4, 5, 6, 7], type: '日常'},
         {id: 6, name: '中央竞技场', weekday: [1], type: '日常'},
@@ -42,17 +42,13 @@ export default {
         {id: 13, name: '公会庆典', weekday: [4, 5], type: '公会'},
         {id: 14, name: '攻坚商店', weekday: [4, 5], type: '商店'},
         {id: 15, name: '公会商店', weekday: [4, 5], type: '商店'},
-        {id: 16, name: '探秘瓦尔哈拉', weekday: [], type: '活动', level: 3, startDate: '2020/05/07', endDate: '2020/06/04', content: 'buff宝珠，附魔等'},
-        {id: 17, name: '积分商城', weekday: [], type: '活动', level: 2.5, startDate: '2020/04/23', endDate: '2020/06/04', content: 'buff光环、通行证、赛丽亚的灿烂幸运等'},
-        {id: 18, name: '再战安徒恩', weekday: [], type: '活动', level: 4, startDate: '2020/05/21', endDate: '2020/07/16', content: '引导石，85ss套，100ss武器，灼炎荒古装扮'},
-        {id: 19, name: '冒险家回归', weekday: [], type: '活动', level: 3, startDate: '2020/05/21', endDate: '2020/07/16', content: '12期天空，+10增幅券等'},
-        {id: 20, name: '强者之路：抉择', weekday: [], type: '活动', level: 3, startDate: '2020/05/21', endDate: '2020/07/16', content: '新增匹配模式，武器装扮，光环，护石符文，袖珍罐等'},
-        {id: 21, name: '逆转的次元', weekday: [], type: '活动', level: 2, startDate: '2020/05/21', endDate: '2020/06/18', content: '动漫联动远古&异界地下城，附魔，灵魂武器，称号，边框等'},
-        {id: 22, name: '幸运卡券', weekday: [], type: '活动', level: 5, startDate: '2020/05/21', endDate: '2020/06/04', content: '引导石，强化券'},
-        {id: 23, name: '智慧之光', weekday: [], type: '活动', level: 5, startDate: '2020/05/21', endDate: '2020/06/18', content: '引导石'},
-        {id: 24, name: '公会你我他', weekday: [], type: '活动', level: 1, startDate: '2020/05/21', endDate: '2020/06/04', content: '公会硬币'},
-        {id: 25, name: '寻宝乐翻天', weekday: [], type: '活动', level: 2, startDate: '2020/05/21', endDate: '2020/06/18', content: '刮刮乐附魔'}
-      ]
+        {id: 16, name: '再战安徒恩', weekday: [], type: '活动', level: 4, startDate: '2020/05/21', endDate: '2020/07/16', content: '引导石，85ss套，100ss武器，灼炎荒古装扮'},
+        {id: 17, name: '冒险家回归', weekday: [], type: '活动', level: 3, startDate: '2020/05/21', endDate: '2020/07/16', content: '12期天空，+10增幅券等'},
+        {id: 18, name: '强者之路：抉择', weekday: [], type: '活动', level: 3, startDate: '2020/05/21', endDate: '2020/07/16', content: '新增匹配模式，武器装扮，光环，护石符文，袖珍罐等'},
+        {id: 19, name: '周周惊喜乐开怀', weekday: [], type: '活动', level: 5, startDate: '2020/06/18', endDate: '2020/08/13', content: '深渊票，司南，100ss跨界石'},
+        {id: 20, name: '夜市大亨', weekday: [], type: '活动', level: 4, startDate: '2020/06/18', endDate: '2020/07/16', content: '+8增幅券，黄金书等'},
+        {id: 21, name: '卜算运势', weekday: [], type: '活动', level: 5, startDate: '2020/06/18', endDate: '2020/07/16', content: '引导石'},
+        {id: 22, name: '阿拉德探险记第三季', weekday: [], type: '活动', level: 5, startDate: '2020/06/18', endDate: '2020/09/22', content: '龙袍'}]
     }
   },
   methods: {
@@ -90,15 +86,15 @@ export default {
           return [0, 0]
         } else if (rowIndex === 15) {
           return [10, 1]
-        } else if ([16, 17, 18, 19, 20, 21, 22, 23, 24].indexOf(rowIndex) !== -1) {
+        } else if ([16, 17, 18, 19, 20, 21].indexOf(rowIndex) !== -1) {
           return [0, 0]
         }
       } else if (columnIndex === 1) {
-        if ([15, 16, 17, 18, 19, 20, 21, 22, 23, 24].indexOf(rowIndex) !== -1) {
+        if ([15, 16, 17, 18, 19, 20, 21].indexOf(rowIndex) !== -1) {
           return [1, 7]
         }
       } else {
-        if ([15, 16, 17, 18, 19, 20, 21, 22, 23, 24].indexOf(rowIndex) !== -1) {
+        if ([15, 16, 17, 18, 19, 20, 21].indexOf(rowIndex) !== -1) {
           return [0, 0]
         }
       }
